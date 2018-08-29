30d421cc315ac8f09dbc8a1ec78f3dfb50b35799
Allison Howard <alliemhow@gmail.com>
2018-08-29 08:27:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,6 @@ def speak_to_grandma(speak)
   if speak == speak.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif speak == speak.upcase
-    speak.upcase
     return "NO, NOT SINCE 1938!"
   end
 end
